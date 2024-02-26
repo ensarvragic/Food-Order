f2e2362e98bab5f8bbe9b72ebc7399ae5befe7b5
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-02-26 22:53:29 +0100

    fetching data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Meals() {
-    async function fetchMeals(){
-       const response = await fetch('http://localhost:3000/meals');
+  const [loadedMeals, setLoadedMeals] = useState([]);
 
-       if(!response.ok){
-        
-       }
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+      }
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
     }
 
+    fetchMeals();
+  }, []);
+
   return (
-    <ul id='meals'></ul>
-  )
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
 }
