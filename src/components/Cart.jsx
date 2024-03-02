160c9b1badc2f1e4d89307e21889dfd87b81b4b6
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-03-02 10:10:31 +0100

    constructing modal

---
+++ src/components/Cart.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../Store/CartContext";
+import { currencyFormatter } from '../util/formatting'
+import Button from "./UI/Button";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  return (
+    <Modal className="cart">
+      <h2>Your Cart</h2>
+      <ul>
+        {CartContext.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-action">
+        <Button textOnly>Close</Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
