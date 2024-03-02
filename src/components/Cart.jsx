9dcf3786b71e02d5ac36c2a2afe6e62168e6a741
Ensar Vragic <121257473+ensarvragic@users.noreply.github.com>
2024-03-02 10:27:36 +0100

    finished modal successfully

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../Store/CartContext";
-import { currencyFormatter } from '../util/formatting'
+import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import userProgressContext from "../Store/UserProgressContext";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
-  const userProgressCtx = useContext(userProgressContext)
+  const userProgressCtx = useContext(userProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -15,17 +15,17 @@ export default function Cart() {
   );
 
   return (
-    <Modal className="cart" open={true}>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
-        {CartContext.items.map((item) => (
+        {cartCtx.items.map((item) => (
           <li key={item.id}>
             {item.name} - {item.quantity}
           </li>
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
-      <p className="modal-action">
+      <p className="modal-actions">
         <Button textOnly>Close</Button>
         <Button>Go to Checkout</Button>
       </p>
